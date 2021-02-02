c8e6e4fad394b20acdea63e112dc619b3d1f934d
Etienne Papegnies <recon@vlay.org>
2021-02-02 15:13:38 +0100

    added new exercise

--- functions.php
+++ functions.php
@@ -8,4 +8,12 @@ function brak($s) {
     return "[" . $s . "]";
 }
 
+function r($n) {
+    $out = "";
+    for ($i = 0; $i < $n; ++$i) {
+        $out .= ".";
+    }
+    return $out;
+}
+
 ?>
--- index.php
+++ index.php
@@ -4,11 +4,13 @@ require_once "functions.php";
 
 echo "Salut\n";
 echo "Appel de f1: " . (f1() + 1) . "\n";
+echo brak("v") . "\n";
 
-// TODO: change line below so it prints "[v]"
-// instead of "v". You can only edit the line
-// below. The line below cannot have the
-// characters "[]"
-echo "v\n";
+// Ecrire deux lignes qui vont afficher respectivement:
+// "..."
+// "......."
+// Ces deux lignes ne peuvent comporter qu'un
+// seul caractère "." chacune. 
+
 
 ?>
