8dcd21413b2c7562dbb21f0dfc89c41336f8ef42
Etienne Papegnies <recon@vlay.org>
2021-02-02 15:24:05 +0100

    completed new exercise with the dots and added following exercise.

--- index.php
+++ index.php
@@ -4,13 +4,16 @@ require_once "functions.php";
 
 echo "Salut\n";
 echo "Appel de f1: " . (f1() + 1) . "\n";
-echo brak("v") . "\n";
+echo brak("v") . "\n"; 
+echo r(3) . "\n";
+echo r(7) . "\n";
 
 // Ecrire deux lignes qui vont afficher respectivement:
-// "..."
-// "......."
+// "---"
+// "-----"
 // Ces deux lignes ne peuvent comporter qu'un
-// seul caractère "." chacune. 
-
+// seul caractère "-" chacune.
+// Vous devez pour cela rajouter une fonction
+// dans functions.php et l'utiliser.
 
 ?>
