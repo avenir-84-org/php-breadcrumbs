66e3b0df758d5dd55ceb4c4d0632acc02facb471
Etienne Papegnies <recon@vlay.org>
2021-02-02 15:00:06 +0100

    fixed mistake and added more instructions

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ echo "Salut\n";
 echo "Appel de f1: " . (f1() + 1) . "\n";
 
 // TODO: change line below so it prints "[v]"
-// instead of "v".
+// instead of "v". You can only edit the line
+// below. The line below cannot have the
+// characters "[]"
 echo "v\n";
 
 ?>
